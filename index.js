9775767f8f361ac0766b0a8261dae856c8f41dd8
Romain Avalle <romain.avalle@lg2.com>
2023-07-11 16:43:49 -0400

    parse route.json

--- index.js
+++ index.js
@@ -17,47 +17,69 @@ async function checkHeadersFile() {
 }
 
 export default async function createDatoMap() {
-  return await this.nuxt.hook("build:done", async (nuxt) => {
-    console.log('dato route map - START!')
-    const array = [];
-    console.log(nuxt.routes);
-    nuxt.routes.forEach((route) => {
-      const entryArray = Object.entries(linkResolverJSON).find(
-        (entry) =>
-          (entry[1].name === route.name) |
-          (entry[1].name === route.name.split("___")[0])
-      );
+  return await this.nuxt.hook("generate:before", async () => {
+    await fs.readFile("./.nuxt/routes.json", "utf8", async (err, data) => {
+      try {
+        const array = [];
+        const jsonData = JSON.parse(data);
 
-      if (entryArray) {
-        const entry = {
-          path: route.path,
-          params: [],
-          model: entryArray[0],
-        };
-        if (entryArray[1].params) {
-          Object.entries(entryArray[1].params).forEach((param) => {
-            entry.params.push(param[1]);
-            entry.path = entry.path.replace(param[0], param[1]);
-          });
-        }
-        if (route.name.includes(`___`)) {
-          entry.locale = route.name.split(`___`)[1];
-        }
-        array.push(entry);
+        jsonData.forEach((route) => {
+          const entryArray = Object.entries(linkResolverJSON).find(
+            (entry) =>
+              (entry[1].name === route.name) |
+              (entry[1].name === route.name.split("___")[0])
+          );
+
+          if (entryArray) {
+            const entry = {
+              path: route.path,
+              params: [],
+              model: entryArray[0],
+            };
+            if (entryArray[1].params) {
+              Object.entries(entryArray[1].params).forEach((param) => {
+                entry.params.push(param[1]);
+                entry.path = entry.path.replace(param[0], param[1]);
+              });
+            }
+            if (route.name.includes(`___`)) {
+              entry.locale = route.name.split(`___`)[1];
+            }
+            array.push(entry);
+          }
+        });
+        const filePath = "./static/dato-route-map.json";
+        console.log(array);
+        // Check if the file exists
+        await fs.access(filePath, fs.constants.F_OK, async (err) => {
+          if (!err) {
+            // File exists, so delete it
+            fs.unlink(filePath, (err) => {
+              if (err) {
+                console.error("Error deleting file:", err);
+              } else {
+                console.log("File deleted successfully.");
+              }
+            });
+          } else {
+            return await fs.writeFile(
+              filePath,
+              JSON.stringify(array),
+              (err) => {
+                if (err) {
+                  console.error(err);
+                }
+              }
+            );
+          }
+        });
+        await checkHeadersFile();
+        console.log("dato route map - DONE!");
+        return true;
+      } catch (parseError) {
+        return false;
       }
     });
     // write a function that check if the file ./static/headers exists. If it exists check if the file as already a ligne with '/dato-route-map.json' if not add it. If the file do not exist create it and add the ligne '/dato-route-map.json' in it.
-    await fs.writeFile(
-      "./static/dato-route-map.json",
-      JSON.stringify(array),
-      (err) => {
-        if (err) {
-          console.error(err);
-        }
-      }
-    );
-    await checkHeadersFile();
-    console.log('dato route map - DONE!')
-    return true;
   });
 }
